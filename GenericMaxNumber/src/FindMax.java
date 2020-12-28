0e28a1398c03b11b518b3e22809ac9a98d6c9331
manassogale132 <73747525+manassogale132@users.noreply.github.com>
2020-12-28 19:58:01 +0530

    Generic class extending to Comparable with generic variables and method.

--- GenericMaxNumber/src/FindMax.java
+++ GenericMaxNumber/src/FindMax.java
@@ -2,7 +2,6 @@ public class FindMax {
 
 	public static void main(String[] args) {
 		
-		
 		MyGeneric<Integer> intPosOne = new MyGeneric<Integer>(86, 38, 56);            //Test 1
 		System.out.println(intPosOne.maximumMethod());
 		 
@@ -25,4 +24,41 @@ public class FindMax {
 		System.out.println(StringPosThree.maximumMethod());
 	
     }
+}
+
+
+class MyGeneric<T extends Comparable<T>> {         //Generic class extending to comparable
+	
+	T one;                                               //Generic variables
+	T two;
+	T three;
+
+	
+    public MyGeneric(T one, T two, T three) {
+		super();
+		this.one = one;
+		this.two = two;
+		this.three = three;
+	}
+
+
+	public T maximumMethod() {  //Return type of this method is <T> i.e Generic type with 3 parameters.
+		
+		T maximum =  one;                   //1 : Initially we assume that 'maximum' is 'one'.
+		
+		if(two.compareTo(one) > 0 ) {       //2 : Then we use compareTo() method to test 'two' with previous 'maximum'-(i.e one).
+			
+			maximum = two;                  //Now updated maximum is 'two'.
+		}
+		
+		if(three.compareTo(maximum) > 0) {  //3 : Then we use compareTo() method to test 'three' with previous 'maximum'-(i.e two).
+			
+			maximum = three;                //Now updated maximum is 'three'.
+		}
+		
+		return maximum;
+	                   		
+	}
+    
+    
 }
