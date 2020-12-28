59a39225cfeb36709b3c81db5fad9c1f479937e3
manassogale132 <73747525+manassogale132@users.noreply.github.com>
2020-12-28 19:51:30 +0530

    Current progress.

--- GenericMaxNumber/src/FindMax.java
+++ GenericMaxNumber/src/FindMax.java
@@ -1,60 +1,28 @@
-public class FindMax {
+public class FindMaxMain {
 
 	public static void main(String[] args) {
 		
-		//one = 86 ,two = 36,three = 52;  (Maximum Number is at 1st Position).
-		System.out.println(maximumMethod(86,36,52));     
 		
-		//one = 36 ,two = 86,three = 52;  (Maximum Number is at 2nd Position)
-		System.out.println(maximumMethod(36,86,52));     
+		MyGeneric<Integer> intPosOne = new MyGeneric<Integer>(86, 38, 56);            //Test 1
+		System.out.println(intPosOne.maximumMethod());
+		 
+		MyGeneric<Integer> intPosTwo = new MyGeneric<Integer>(56, 86, 38);            //Test 2
+		System.out.println(intPosTwo.maximumMethod());
 		
-		//one = 52 ,two = 36,three = 86;  (Maximum Number is at 3rd Position).
-		System.out.println(maximumMethod(52,36,86));  
-		
-	//---------------------------------------------------------------------------------------------------------------------------------------------------	
-		
-		//one = 8.31f ,two = 4.31f,three = 5.99f;  (Maximum Number is at 1st Position).
-		System.out.println(maximumMethod("8.31f","4.31f","5.99f"));     
-		
-		//one = 4.31f ,two = 8.31f,three = 5.99f;  (Maximum Number is at 2nd Position)
-		System.out.println(maximumMethod("4.31f","8.31f","5.99f"));     
-		
-		//one = 5.99f ,two = 4.31f,three = 8.31f;  (Maximum Number is at 3rd Position).
-		System.out.println(maximumMethod("5.99f","4.31f","8.31f"));  
+		MyGeneric<Integer> intPosThree = new MyGeneric<Integer>(56, 86, 38);          //Test 3
+		System.out.println(intPosThree.maximumMethod());
 	
-	//---------------------------------------------------------------------------------------------------------------------------------------------------	
-
+		System.out.println("  ");
+		//--------------------------------------------------------------------------------------------------------
 		
-		//one = Peach ,two = Banana ,three = Apple;  (Maximum Number is at 1st Position).
-		System.out.println(maximumMethod("Peach","Banana","Apple"));     
+		MyGeneric<String> StringPosOne = new MyGeneric<String>("Peach", "Apple", "Banana");     //Test 1
+		System.out.println(StringPosOne.maximumMethod());
 		
-		//one = Apple ,two = Peach ,three = Banana;  (Maximum Number is at 2nd Position)
-		System.out.println(maximumMethod("Apple","Peach","Banana"));     
+		MyGeneric<String> StringPosTwo = new MyGeneric<String>("Banana", "Peach", "Apple");     //Test 2
+		System.out.println(StringPosTwo.maximumMethod());
 		
-		//one = Banana ,two = Apple ,three = Peach;  (Maximum Number is at 3rd Position).
-		System.out.println(maximumMethod("Banana","Apple","Peach"));  
-		
-	}
+		MyGeneric<String> StringPosThree = new MyGeneric<String>("Banana", "Apple", "Peach");   //Test 3
+		System.out.println(StringPosThree.maximumMethod());
 	
-	
-	//Generic Method.
-	public static <T extends Comparable<T>> T maximumMethod(T one,T two,T three) {  //Return type of this method is <T> i.e Generic type with 3 parameters.
-		
-		T maximum =  one;                   //1 : Initially we assume that 'maximum' is 'one'.
-		
-		if(two.compareTo(one) > 0 ) {       //2 : Then we use compareTo() method to test 'two' with previous 'maximum'-(i.e one).
-			
-			maximum = two;                  //Now updated maximum is 'two'.
-		}
-		
-		if(three.compareTo(maximum) > 0) {  //3 : Then we use compareTo() method to test 'three' with previous 'maximum'-(i.e two).
-			
-			maximum = three;                //Now updated maximum is 'three'.
-		}
-		return maximum;                     //Finally returning the 'maximum'.
-				
-	}
-	
-	
-
+    }
 }
